c68a95ba0c47bd2f7e018ab457d38a86fa8a49cb
Myro Joy Lee <myro.joy.olida.092282@gmail.com>
2023-05-13 01:54:38 -0400

    added parent shape class and children shapes classes

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 //                     MODULES/DEPENDENCIES
 // ======================================================== //
 
-const shape = require("./lib/shapes");
+const Shape = require("./lib/shapes");
+const fs = require("fs");
 const inquirer = require("inquirer");
 const MaxLength = require("inquirer-maxlength-input-prompt");
 
@@ -31,21 +32,52 @@ const shapeQuestions = [
     maxLength: 3,
   },
 ];
+const sampleShape = `
+    <svg
+    width="541"
+    height="271"
+    >
+
+        <rect
+            width="541"
+            height="271"
+            x="0"
+            y="0"
+            fill="#3d5ea1"
+        />
+
+        <polygon
+            fill="white"
+            points="100,50 400,100 320,200 80,230"
+        />
+
+    </svg>
+    `;
 let userData;
 // ======================================================== //
 //                          CODE BELOW
 // ======================================================== //
-function init() {
-  console.log(`\nHello! Welcome to the Shape Generator!\n
-    Everything you type is restricted to 3 characters.
-    Just follow the directions and we will get a shape in the end!\n`);
+// function init() {
+//   console.log(`\nHello! Welcome to the Shape Generator!\n
+//     Everything you type is restricted to 3 characters.
+//     Just follow the directions and we will get a shape in the end!\n`);
 
-  inquirer.registerPrompt("maxlength-input", MaxLength);
-  inquirer.prompt(shapeQuestions).then((answers) => next(answers));
-}
-init();
+//   inquirer.registerPrompt("maxlength-input", MaxLength);
+//   inquirer.prompt(shapeQuestions).then((answers) => next(answers));
+// }
+// init();
 
-function next(answers) {
-  userData = answers;
-  console.log(`The user wants a ${userData.textColor} ${userData.shape}.`);
-}
+// function next(answers) {
+//   userData = answers;
+//   console.log(`The user wants a ${userData.textColor} ${userData.shape}.`);
+// }
+
+// fs.writeFile("sample-file.svg", sampleShape, (err) => {
+//   if (err) {
+//     return console.err(err);
+//   }
+//   console.log("The file was saved!");
+// });
+
+const renderThis = new Triangle();
+console.log(renderThis.render());
